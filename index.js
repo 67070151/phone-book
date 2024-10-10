ff3f382970719088f84f99e59d2e0f303c04b6bd
67070151 <67070151@kmitl.ac.th>
2024-10-11 01:33:42 +0700

    can change name

---
+++ index.js
@@ -0,0 +1,9 @@
+function change_username() {
+    let username = document.getElementById("username_input").value;
+    document.getElementById("username").innerHTML = username;
+}
+
+function change_url() {
+    let url = document.getElementById("url_ipnut").value;
+    document["pic1"].src = searchPic.src;
+}
